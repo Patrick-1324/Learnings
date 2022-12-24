e979eae9eb0ca39657f468462e6e5fa6cbc950fb
Prateek <pkjoshi1997@gmail.com>
2022-12-24 08:47:57 +0530

    Khan Academy Tutorials

---
+++ SQL/SQL_Tutorials/khan_academy_1.sql
@@ -0,0 +1,16 @@
+
+/** 
+***** CREATING A TABLE AND INSTERTING DATA *****
+Grocery List:
+Bananas (6)
+Peanut Butter (1)
+Dark Chocolate Bars (4)
+**/
+/** create table and define properties **/
+CREATE TABLE groceries(id INTEGER primary KEY, name TEXT, quantity INTEGER);
+/** insert data to tables **/
+INSERT INTO groceries VALUES (1, "Bananas", 6);
+INSERT INTO groceries VALUES (2, "Peanut Butter", 1);
+INSERT INTO groceries VALUES (3, "Chocolate Bars", 3);
+/** run querry to view table groceries**/
+SELECT * FROM groceries;
